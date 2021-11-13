949897a803ac7c7b7749fb9ea1b2cab0f4d87ea1
Felipe <fepipe82@hotmail.com>
2021-11-13 16:09:58 -0300

    Lazyload basico listo

--- src/01-lazyload/pages/LazyPage1.tsx
+++ src/01-lazyload/pages/LazyPage1.tsx
@@ -5,3 +5,5 @@ export const LazyPage1 = () => {
       <h1>LazyPage1</h1>
   )
 }
+
+export default LazyPage1;
--- src/01-lazyload/pages/LazyPage3.tsx
+++ src/01-lazyload/pages/LazyPage3.tsx
@@ -5,3 +5,5 @@ export const LazyPage3 = () => {
       <h1>LazyPage3</h1>
   )
 }
+
+export default LazyPage3;
